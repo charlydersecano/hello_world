1530e866b3117fa07cb16fdff9da22d2815c4f94
charlydersecano <itidio34ler>
2021-02-23 17:24:27 +0100

    tipos de datos

---
+++ 00_generic.py
@@ -0,0 +1,7 @@
+# asi escribimos lo que queramos que no ejecutara
+"""
+esto tampoco se ejecutara pero solo sirve para colocar en otra linea
+"""
+print('carlos')
+# print('que')
+# print('dices')
---
+++ 01_tipos_dato.py
@@ -0,0 +1,22 @@
+# hagamos cuentas
+numero_peque = 1
+numero_grande = 8
+sumatory = + numero_grande
+print(sumatory)
+restilla = numero_peque - numero_grande
+print(restilla)
+numero_raro = 3.3
+resto_raro = numero_peque - numero_raro + numero_grande
+print(resto_raro)
+destreza = 7
+conocimiento_de_armas = 2
+manejo_de_espada = (destreza + conocimiento_de_armas) / 2
+print(manejo_de_espada)
+print(numero_raro)
+print('ataca la liberlula y te defiendes con', manejo_de_espada)
+print(int(manejo_de_espada))
+print(type(destreza))
+colega = type(numero_raro)
+print(colega)
+texto1 = 'mi habilidad con el arma es'
+print(texto1 + ' ' + str(manejo_de_espada))
